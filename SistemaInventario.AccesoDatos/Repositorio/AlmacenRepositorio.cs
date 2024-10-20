0cee2f25d91d60085662038d0a6b8f609d91d9c8
Carlos Ayala <ing.full.stack@hotmail.com>
2024-10-20 06:43:53 -0400

    Patron de Repositorio

---
+++ SistemaInventario.AccesoDatos/Repositorio/AlmacenRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class AlmacenRepositorio : Repositorio<Almacen>, IAlmacenRepositorio
+    {
+        private readonly ApplicationDbContext db;
+
+        public AlmacenRepositorio(ApplicationDbContext db) : base(db)
+        {
+            this.db = db;
+        }
+
+        public async Task Actualizar(Almacen almacen)
+        {
+            var obj = await db.Almacenes.FirstOrDefaultAsync(x => x.Id == almacen.Id);
+
+            if (obj != null)
+            {
+                obj.Nombre = almacen.Nombre;
+                obj.Descripcion = almacen.Descripcion;
+                obj.Estado = almacen.Estado;
+            }
+        }
+    }
+}
---
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IAlmacenRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using SistemaInventario.Modelo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface IAlmacenRepositorio : IRepositorio<Almacen>
+    {
+        Task Actualizar(Almacen almacen);
+    }
+}
